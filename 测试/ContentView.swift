a5e5787820148a6a93255d5515bfd8d427750042
ZhouKunLin <1054023430a@gmail.com>
2022-12-11 16:35:02 +0800

    Initial Commit

---
+++ 测试/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  测试
+//
+//  Created by 周坤琳 on 2022/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 测试/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  测试
+//
+//  Created by 周坤琳 on 2022/12/11.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
